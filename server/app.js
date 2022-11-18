c281d75c0d66f91e791f8ec03ae939d6dac74ff2
Edwin00821 <eastudillop2000@alumno.ipn.mx>
2022-11-17 22:08:16 -0600

    Change url cors origin

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.static(join(__dirname, "../client/build")));
 const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-ten-rho.vercel.app/",
   },
 });
 
